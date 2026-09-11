f03751cad4ae7af133948de2114646623eb87c1b
ssl-101 <inquiresiham@gmail.com>
2026-09-11 18:44:46 -0400

    player method added

--- src/RPS.cpp
+++ src/RPS.cpp
@@ -13,6 +13,20 @@ std::string Computer::getChoice() {
     if (val == 1) return "rock";
     if (val == 2) return "paper";
     return "scissors";
-    
 }
 
+ // Player method code
+ std::string Player::getChoice() {
+    std::string choice;
+    std::cout << "Choose rock, paper, scissors, or quit:";
+    std::cin >> choice;
+
+    while (choice != "rock" && choice != "paper" 
+        && choice != "scissors" && choice != "quit"){
+        std::cout << "Invalid Choice. Please enter rock , paper, scissors or quit:";
+        std::cin >> choice;
+        }
+      
+ 
+return choice;
+    }
